a49f263b6ce0b0a262c3af7565198cb6e3d7dcc8
barbosaoli <gustavobarbosa7162@gmail.com>
2022-02-07 16:49:28 -0300

    implements update method

--- src/controllers/statementsController.js
+++ src/controllers/statementsController.js
@@ -3,6 +3,54 @@ import dayjs from "dayjs";
 import { ObjectId } from "mongodb";
 import db from "../db.js";
 
+export async function updateStatement(req, res) {
+  const { id } = req.params;
+  const { value, description } = req.body;
+  const { authorization } = req.headers;
+  const token = authorization?.replace("Bearer ", "");
+
+  if (!token) return res.sendStatus(401);
+
+  try {
+    const session = await db.collection("sessions").findOne({ token });
+
+    if (!session) return res.sendStatus(401);
+
+    const dbUser = await db.collection("users").findOne({
+      _id: session.userId,
+    });
+
+    const statement = await db
+      .collection("statements")
+      .findOne({ _id: new ObjectId(id) });
+
+    if (!statement) return res.sendStatus(401);
+    console.log(id);
+
+    await db
+      .collection("statements")
+      .updateOne(
+        {
+          _id: new ObjectId(id),
+        },
+        {
+          $set: {
+            idUser: dbUser._id,
+            value,
+            description,
+            date: dayjs().locale("pt-br").format("DD/MM/YY"),
+          },
+        }
+      )
+      .then((res) => console.log(res))
+      .catch((res) => console.log(res));
+
+    res.sendStatus(201);
+  } catch (error) {
+    res.sendStatus(401);
+  }
+}
+
 export async function deleteStatement(req, res) {
   const { id } = req.params;
 
--- src/routes/statementsRouter.js
+++ src/routes/statementsRouter.js
@@ -3,12 +3,14 @@ import {
   statements,
   newEntry,
   deleteStatement,
+  updateStatement,
 } from "../controllers/statementsController.js";
 import statementsMiddleware from "../middlewares/statementsMiddleware.js";
 
 const statementsRouter = Router();
 
 statementsRouter.get("/statements/:id", statements);
+statementsRouter.put("/statements/:id", updateStatement);
 statementsRouter.delete("/statements/:id", deleteStatement);
 statementsRouter.post("/new-entry", statementsMiddleware, newEntry);
 
