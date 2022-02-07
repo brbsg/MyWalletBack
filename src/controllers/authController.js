4f97c2defdc7c69ee354e2d961301d0b00ed1f28
barbosaoli <gustavobarbosa7162@gmail.com>
2022-02-07 11:16:28 -0300

    increases auth

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -8,6 +8,10 @@ export async function signUp(req, res) {
   const passwordHash = bcrypt.hashSync(user.password, 10);
 
   try {
+    const dbUser = await db.collection("users").findOne({ email: user.email });
+
+    if (dbUser) return res.sendStatus(401);
+
     await db.collection("users").insertOne({ ...user, password: passwordHash });
 
     res.sendStatus(201);
@@ -30,7 +34,7 @@ export async function signIn(req, res) {
         token,
       });
 
-      res.send({ id: dbUser._id, token }).status(201);
+      res.send({ id: dbUser._id, name: dbUser.name, token }).status(201);
     } else {
       res.sendStatus(401);
     }
